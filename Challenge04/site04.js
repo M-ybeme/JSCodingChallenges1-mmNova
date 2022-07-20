9fdb1c1e2ed9b7b32fa6aaed182afd15b3f58821
M-ybeme <marlomayberry.90@gmail.com>
2022-07-20 14:48:28 -0400

    js challenges

--- Challenge04/site04.js
+++ Challenge04/site04.js
@@ -126,10 +126,12 @@ let cityData = [{
 function citySort() {
 
     
-    sortByPopulation(cityData, "desc");
+   // sortByPopulation(cityData, "desc");
+
+    
     
     //extra credit functions
-    //sortyByName(cityData);
+    sortyByName(cityData, "desc");
     //sortByAge(cityData, "asc");
     
     //used for display purposes. not need to change
@@ -145,15 +147,52 @@ function citySort() {
 
 //takes an array of objects and sorts by population. 
 function sortByPopulation(cityData, sortDir) {
+
+    if (sortDir = "asc") {
+        cityData.sort((a, b) => {
+            return (a.population - b.population);
+        });
+    }
+    else {
+        cityData.sort((a, b) => {
+            return (b.population - a.population);
+        });
+    }
+    
     
 }
 
 //takes an array of objects and sorts by median age. 
-function sortByAge(cityData, sortDir){
-  
+function sortByAge(cityData, sortDir) {
+    if (sortDir = "asc") {
+        cityData.sort((a, b) => {
+            return (a.median_age - b.median_age);
+        });
+    } else {
+        cityData.sort((a, b) => {
+            return (b.median_age - a.median_age);
+        });
+        }
 }
 
 //takes an array of objects and sorts by city name. 
-function sortyByName(cityData) {
-    
-}
+function sortyByName(cityData, sortDir) {
+    cityData.sort((a, b) => {
+        let ca = a.city.toLowerCase();
+        let cb = b.city.toLowerCase();
+
+        let compare = 0;
+        if (ca > cb) {
+            compare = 1;
+        } else if
+            (ca < cb) {
+            compare = -1;
+        }
+        // ascending order(A-Z)
+        if (sortDir == "asc") {
+            return compare;
+        } else {
+            return compare * -1;
+        }
+        });
+    }
--- Challenge08/site08.js
+++ Challenge08/site08.js
@@ -8,11 +8,11 @@ function displayNumbers() {
     let avg = avgNumber(numbers);
     let max = maxNumber(numbers);
     let min = minNumber(numbers);
-    let sumRec = sumNumbersRecursive(numbers,index);
+    //let sumRec = sumNumbersRecursive(numbers,index);
 
     //used for display not need to change unless you are doing only the sum function
     let msg = `Sum = ${sum}<br>Avg = ${avg.toFixed(2)}<br>Max = ${max}<br>Min = ${min}`;
-    
+    let msg2 = `Sum = "${sum}"`;
     //display the message    
     document.getElementById("results").innerHTML = msg;
     document.getElementById("numberslist").innerHTML = numbers.toString();
@@ -20,28 +20,59 @@ function displayNumbers() {
 
 //takes an array of numbers and returns the sum of all the numbers
 function sumNumbers(numArry) {
-    return 0;
+    let sum = 0;
+    for (let index = 0; index < numArry.length; index++) {
+        sum += numArry[index];
+        
+    }
+
+
+    return sum;
 }
 
 //takes an array of numbers and the current index in the array and returns the sum of all the numbers using recursion
 //Recursion is the technique of the function calling itself. 
 //NOTE * SEE CHALLENGE 11. 
 function sumNumbersRecursive(numArry, index) {
-    return 0;
 }
 
 
 //takes an array of numbers and returns the average of all the numbers
 function avgNumber(numArry) {
-    return 0;
+    let avg = 0;
+    let sum = 0;
+    for (let index = 0; index < numArry.length; index++) {
+        sum += numArry[index];
+    }
+     avg = sum / numArry.length
+
+    return avg;
 }
 
 //takes an array of numbers and returns the max number
 function maxNumber(numArry) {
-    return 0;
+    let max = 0;
+    for (let index = 0; index < numArry.length; index++) {
+        if (max < numArry[index]) {
+            max = numArry[index];
+        }
+        
+    }
+
+
+    return max;
 }
 
 //takes an array of numbers and returns the min number
 function minNumber(numArry) {
-    return 0;
+
+    min = 100;
+    for (let index = 0; index < numArry.length; index++) {
+        if (min > numArry[index]) {
+            min = numArry[index];
+        }
+        
+    }
+
+    return min;
 }
